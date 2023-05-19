3fd348f9a39d600e516774c4725f0b393ce2b53f
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-05-19 13:06:07 +0700

    Express Serve Static Files

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express'),
 
 const hostname = 'localhost';
 const port = 3000;
-
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
